e6d273a532049ef8841d181707a476bc9fd1b2d1
Daniel Bielaszka <danielbielaszka@gmail.com>
2024-04-08 20:49:36 +0200

    set initial m and v values for AdamOptimizer

--- optimizers.py
+++ optimizers.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Optimizer:
     def __init__(self, learning_rate):
         self.learning_rate = learning_rate
@@ -7,54 +10,48 @@ class Optimizer:
 
 
 class AdamOptimizer(Optimizer):
-    def __init__(self, learning_rate, beta1=0.9, beta2=0.999, epsilon=1e-8):
-        super().__init__(learning_rate)
+
+    def __init__(self, learning_rate=0.001, beta1=0.9, beta2=0.999, epsilon=1e-8):
+        self.learning_rate = learning_rate
         self.beta1 = beta1
         self.beta2 = beta2
         self.epsilon = epsilon
+        self.t = 0
         self.m = []
         self.v = []
-        self.t = 0
 
     def update(self, neural_network):
-        if not self.m:
-            for i in range(1, len(neural_network.layers)):
-                self.m.append(
-                    [
-                        [0.0 for _ in range(neural_network.layers[i - 1])]
-                        for _ in range(neural_network.layers[i])
-                    ]
-                )
-                self.v.append(
-                    [
-                        [0.0 for _ in range(neural_network.layers[i - 1])]
-                        for _ in range(neural_network.layers[i])
-                    ]
-                )
-
         self.t += 1
-        for i in range(1, len(neural_network.layers)):
-            for j in range(neural_network.layers[i]):
-                for k in range(neural_network.layers[i - 1]):
-                    self.m[i - 1][j][k] = (
-                        self.beta1 * self.m[i - 1][j][k]
-                        + (1 - self.beta1)
-                        * neural_network.delta[i][j]
-                        * neural_network.neurons[i - 1][k]
+        if not self.m:
+            self.m = [
+                [
+                    [0.0 for _ in range(len(neural_network.neurons[i - 1]))]
+                    for _ in range(len(neural_network.neurons[i]))
+                ]
+                for i in range(len(neural_network.weights))
+            ]
+            self.v = [
+                [
+                    [0.0 for _ in range(len(neural_network.neurons[i - 1]))]
+                    for _ in range(len(neural_network.neurons[i]))
+                ]
+                for i in range(len(neural_network.weights))
+            ]
+
+        for i in range(len(neural_network.weights) - 1, 0, -1):
+            for j in range(len(neural_network.neurons[i])):
+                for k in range(len(neural_network.neurons[i - 1])):
+                    g = -neural_network.delta[i][j] * neural_network.neurons[i - 1][k]
+
+                    self.m[i][j][k] = (
+                        self.beta1 * self.m[i][j][k] + (1 - self.beta1) * g
                     )
-                    self.v[i - 1][j][k] = (
-                        self.beta2 * self.v[i - 1][j][k]
-                        + (1 - self.beta2)
-                        * (
-                            neural_network.delta[i][j]
-                            * neural_network.neurons[i - 1][k]
-                        )
-                        ** 2
+                    self.v[i][j][k] = (
+                        self.beta2 * self.v[i][j][k] + (1 - self.beta2) * g * g
                     )
-                    m_hat = self.m[i - 1][j][k] / (1 - self.beta1**self.t)
-                    v_hat = self.v[i - 1][j][k] / (1 - self.beta2**self.t)
-                    neural_network.weights[i][j][k] -= (
-                        self.learning_rate * m_hat / (v_hat**0.5 + self.epsilon)
+                    m_hat = self.m[i][j][k] / (1 - math.pow(self.beta1, self.t))
+                    v_hat = self.v[i][j][k] / (1 - math.pow(self.beta2, self.t))
+
+                    neural_network.weights[i][j][k] += (
+                        self.learning_rate * m_hat / (math.sqrt(v_hat) + self.epsilon)
                     )
-                self.m[i - 1][j] = [0.0 for _ in range(neural_network.layers[i - 1])]
-                self.v[i - 1][j] = [0.0 for _ in range(neural_network.layers[i - 1])]
