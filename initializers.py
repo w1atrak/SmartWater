32faf15e06cbd084191b49d0730b032a4cb98140
Daniel Bielaszka <danielbielaszka@gmail.com>
2024-04-13 20:48:50 +0200

    add FileInitializer class to initializers.py

--- initializers.py
+++ initializers.py
@@ -1,4 +1,5 @@
 import random
+import csv
 
 
 class Initializer:
@@ -7,5 +8,23 @@ class Initializer:
 
 
 class RandomInitializer(Initializer):
+
     def initialize(self):
         return random.uniform(-0.5, 0.5)
+
+
+class FileInitializer(Initializer):
+    weights = []
+
+    def __init__(self):
+        with open("weights.csv", "r") as file:
+            csv_reader = csv.reader(file)
+            for row in csv_reader:
+                for v in list(map(float, row)):
+                    self.weights.append(v)
+
+    def initialize(self, randomized=True):
+        if randomized:
+            return random.uniform(-0.5, 0.5)
+        else:
+            return self.weights.pop(0)
