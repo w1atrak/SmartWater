7067ac02e87f7e6dcf7eb2ac2179c2d499848992
Daniel Bielaszka <danielbielaszka@gmail.com>
2024-04-30 16:32:48 +0200

    handle missing weights.csv file gracefully

--- initializers.py
+++ initializers.py
@@ -17,14 +17,17 @@ class FileInitializer(Initializer):
     weights = []
 
     def __init__(self):
-        with open("weights.csv", "r") as file:
-            csv_reader = csv.reader(file)
-            for row in csv_reader:
-                for v in list(map(float, row)):
-                    self.weights.append(v)
+        try:
+            with open("weights.csv", "r") as file:
+                csv_reader = csv.reader(file)
+                for row in csv_reader:
+                    for v in list(map(float, row)):
+                        self.weights.append(v)
+        except FileNotFoundError:
+            pass
 
     def initialize(self, randomized=True):
         if randomized:
-            return random.uniform(-0.5, 0.5)
+            return 0.3  # random.uniform(-0.5, 0.5)
         else:
-            return self.weights.pop(0)
+            return self.weights.pop(0) if self.weights else random.uniform(-0.5, 0.5)
