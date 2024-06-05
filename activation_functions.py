794a719dfa731cb728427a5fc5c7b0949fd3fefe
Daniel Bielaszka <danielbielaszka@gmail.com>
2024-06-05 23:04:25 +0200

    add Sigmoid and TanH activation functions

--- activation_functions.py
+++ activation_functions.py
@@ -15,3 +15,19 @@ class ReLU(ActivationFunction):
 
     def derivative(self, x):
         return 1.0 if x > 0.0 else 0.0
+
+
+class Sigmoid(ActivationFunction):
+    def activate(self, x):
+        return 1.0 / (1.0 + pow(2.718281828459045, -x))
+
+    def derivative(self, x):
+        return self.activate(x) * (1 - self.activate(x))
+
+
+class TanH(ActivationFunction):
+    def activate(self, x):
+        return 2.0 / (1.0 + pow(2.718281828459045, -2 * x)) - 1.0
+
+    def derivative(self, x):
+        return 1 - pow(self.activate(x), 2)
